30a21a61c995a579c0c7e9d21cc61d68dce02c06
Yuri Ramos <yurisouzaramos10@gmail.com>
2026-03-22 00:39:12 -0300

    origin

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 import sys
 
+     #FUNCOES
 def escrever(texto):
     for letra in texto:
         sys.stdout.write(letra)
@@ -10,10 +11,16 @@ def escrever(texto):
         time.sleep(0.05)  # velocidade (menor = mais rápido)
     print()  # quebra de linha no final
 
+def input_animado(texto):
+    escrever(texto)
+    return input()
+
 def limpar(): #Faz a troca de tela
     os.system("cls")
 
-
+     #Script principal
+limpar()
+     
 print('=' * 20)
 print('BEM VINDO AO: E SE ?')
 print('=' * 20)
@@ -30,11 +37,37 @@ if menu == '1':
     #SAMPLE ESCOLHE SEM REPETIR O ELEMENTO // CHOICE PODE REPETIR O ELEMENTO
 
     print(f'Voce vai ao Mercado ! \nChegando la voce olha a sua lista: {alimEscolhidos}')
+    inp = input_animado('oque fazer? \n1.Ir fazer suas compras \n2.Ir embora')
 
+    if  inp == '1':
+        #limpar() #deve limpar o terminal (troca de tela)
+        escrever("Andando.......")
+        time.sleep(0.5)
 
+        print('Voce foi no corredor das frutas.')
 
+        verlista = input_animado('Deseja ver a lista ? \n sim/nao').strip().lower()
 
+        if verlista == 'sim':
+            escrever(alimEscolhidos)
 
+        elif verlista == 'nao':
+            escrever('Por enquanto to progrmando o SIM')
+        else:
+            escrever('ERRO Escolha sim ou nao')
+            escrever('Impossivel de Continuar')
+            exit()
+            
+        Tem_Fruta = input_animado('Vamos la!, possui fruta em sua lista ?\n').strip().lower()
+
+        if Tem_Fruta == 'sim':
+            pegarfruta = input_animado('Deseja pegar a fruta ?\n')
+
+        elif Tem_Fruta == 'nao':
+            escrever('Por enquanto to progrmando o SIM')
+        else:
+             print('ERRO Escolha sim ou nao')
+        
 elif menu == '2':
     print('!!AINDA TRABALHANDO NISSO!!')
     exit()
